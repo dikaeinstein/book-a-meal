f35a71863f2d5caac87259896ae11ed0d8f1765d
Solomon Okwa <solozyokwa@gmail.com>
2018-04-19 16:25:52 +0100

    Update and refine UI templates

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,13 +1,19 @@
 window.addEventListener('load', () => {
     const footerYear = document.getElementById('year');
     const menuDate = document.getElementById('date');
-    footerYear.innerText = new Date().getFullYear();
-    menuDate.innerText = new Date().toDateString();
+    const toggleShow = document.querySelector('.toggle-show');
     const toggleNavbar = () => {
-        const navContainer = document.getElementsByClassName('nav-container')[0];
+        const navContainer = document.
+            getElementsByClassName('nav-container')[0];
         navContainer.classList.toggle('navbar-toggle-show');
     };
 
-    document.querySelector('.toggle-show')
-        .addEventListener('click', toggleNavbar);
+    footerYear.innerText = new Date().getFullYear();
+
+    if (menuDate) {
+        menuDate.innerText = new Date().toDateString();
+    }
+    if (toggleShow) {
+        toggleShow.addEventListener('click', toggleNavbar);
+    }
 });
