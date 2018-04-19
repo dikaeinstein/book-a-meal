cd696add7f205fa0cccc48deebf8ebdc7e05dc45
Solomon Okwa <solozyokwa@gmail.com>
2018-04-19 01:57:57 +0100

    Update meals page
    Clean out typo errors and refactor elememts

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', () => {
     const footerYear = document.getElementById('year');
+    const menuDate = document.getElementById('date');
     footerYear.innerText = new Date().getFullYear();
-
+    menuDate.innerText = new Date().toDateString();
     const toggleNavbar = () => {
         const navContainer = document.getElementsByClassName('nav-container')[0];
         navContainer.classList.toggle('navbar-toggle-show');
