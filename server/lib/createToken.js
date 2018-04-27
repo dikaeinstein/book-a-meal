245c7b29be31f6d0e5d027c522b3b69fa2c7df95
Solomon Okwa <solozyokwa@gmail.com>
2018-04-27 06:01:35 +0100

    Update authenticate and createToken

--- server/lib/createToken.js
+++ server/lib/createToken.js
@@ -3,12 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { secret } = process.env;
+// const { secret } = process.env;
 
-const createToken = id => jwt.sign(
-  { id },
-  secret,
-  { expiresIn: '1 day' },
-);
+const secret = 'dikababa1111111111111111111111';
+
+const createToken = (id) => {
+  return jwt.sign(
+    { id },
+    secret,
+    { expiresIn: '1 day' },
+  );
+};
 
 export default createToken;
